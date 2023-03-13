34a7c4cab674e958427418d7e810439c9574699d
HCanArslan <hamitcanarslan1@gmail.com>
2023-03-13 22:20:34 +0300

    test for vercel

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://airbnb-clone-booking-37jwvu7mv-hcanarslan.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://airbnb-clone-azure-eight.vercel.app/');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
